5e504ba369f2bfa4d3e47dad2a71206c247f635b
Andre luiz <andre.bluiz94@gmail.com>
2020-03-30 17:34:53 -0300

    add functinal advencer

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,23 +1,44 @@
-const express = require("express");
+const express = require( "express" );
 
-const OngController = require("../controllers/OngController");
-const IncidentController = require("../controllers/IncidentController");
-const ProfileController = require("../controllers/ProfileController");
-const SessionController = require("../controllers/SessionController");
+const { celebrate, Segments, Joi } = require( 'celebrate' )
+const OngController = require( "../controllers/OngController" );
+const IncidentController = require( "../controllers/IncidentController" );
+const ProfileController = require( "../controllers/ProfileController" );
+const SessionController = require( "../controllers/SessionController" );
 
 const routes = express.Router();
 
-routes.post("/session", SessionController.store);
+routes.post( "/session", SessionController.store );
 
-routes.get("/ongs", OngController.index);
+routes.get( "/ongs", OngController.index );
 
-routes.post("/ongs", OngController.store);
+routes.post( "/ongs", celebrate( {
+    [Segments.BODY]: Joi.object().keys( {
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.string().required().min( 10 ).max( 11 ),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length( 2 )
+    } )
+} ), OngController.store );
 
-routes.get("/profile", ProfileController.index);
+routes.get( "/profile", celebrate( {
+    [Segments.HEADERS]: Joi.object( {
+        authorization: Joi.string().required()
+    } ).unknown()
+} ), ProfileController.index );
 
-routes.post("/incidents", IncidentController.store);
-routes.get("/incidents", IncidentController.index);
+routes.post( "/incidents", IncidentController.store );
+routes.get( "/incidents", celebrate( {
+    [Segments.QUERY]: Joi.object().keys( {
+        page: Joi.number()
+    } )
+} ), IncidentController.index );
 /* routes.put("/incidents/:id", IncidentController.index); */
-routes.delete("/incidents/:id", IncidentController.delete);
+routes.delete( "/incidents/:id", celebrate( {
+    [Segments.PARAMS]: Joi.object().keys( {
+        id: Joi.number().required(),
+    } )
+} ), IncidentController.delete );
 
 module.exports = routes;
