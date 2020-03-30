0d21142a56d67e1e008345170952e4843ad0f64d
Andre luiz <andre.bluiz94@gmail.com>
2020-03-30 17:42:19 -0300

    add directory frontend

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require( "express" );
+const { errors } = require( 'celebrate' )
+const routes = require( "./routes/routes" );
+const cors = require( "cors" );
+
+const app = express();
+
+
+app.use( cors() );
+app.use( express.json() );
+app.use( routes );
+app.use( errors() )
+
+module.exports = app;
